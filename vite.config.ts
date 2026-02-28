125ec5ca96ff900b46c3a352b4bcc5bfa1c71257
hectorrami <hramirez4@uh.edu>
2026-02-28 14:26:19 -0600

    changes for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // <-- Add this import
+import tailwindcss from '@tailwindcss/vite' 
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // <-- Add this to the list
+    tailwindcss(), 
   ],
+  base: '/OneHope/', 
 })
